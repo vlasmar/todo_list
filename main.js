e05c7d5ffba77506cfd2c6fb717b25d6e3788982
Umme Salma <nupur2238@gmail.com>
2022-08-11 16:20:24 +0200

    add save button and update edit button

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createTask(){
     const features = document.createElement('div');
     const edit = document.createElement('img');
     const deleteTask = document.createElement('img');
+    const saveBtn =document.createElement('img')
 
     listItem.classList.add('todo-tasks');
     divTask.classList.add('task');
@@ -15,6 +16,7 @@ function createTask(){
     features.className = 'features';
     edit.className = 'pencil';
     deleteTask.className = 'delete';
+    saveBtn.className='save';
 
     list.appendChild(listItem);
     listItem.appendChild(divTask);    
@@ -23,23 +25,46 @@ function createTask(){
     listItem.append(divTask);    
     listItem.append(features);
     features.append(edit);
+    features.append(saveBtn);
     features.append(deleteTask);
 
     checkbox.type='checkbox';
     info.innerHTML=input.value;
     edit.src = 'images/pencil.png';
     deleteTask.src = 'images/delete-icon.png';
+    saveBtn.src='images/save.jpg';
+    saveBtn.style.display='none';
     
     //document.querySelector('.pencil').onclick = function() {myFunction()};
     edit.addEventListener('click', function() {
         info.contentEditable = true;
-        info.className = 'paragraph';
+        info.classList.toggle('paragraph');
+        edit.style.display='none';
+        saveBtn.style.display='inline';
+       
       } );
+      saveBtn.addEventListener('click',(e) =>  {
+        info.contentEditable=false;
+        //info.innerText=paragraph.value;
+        info.classList.toggle('paragraph');
+        edit.style.display='inline';
+        saveBtn.style.display='none';
+
+      });
     deleteTask.addEventListener('click', (e) => {
         listItem.remove();
     });
 }
 
 let addButton = document.querySelector('#add');
-addButton.addEventListener('click', createTask);
+addButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(input.value===""){
+        alert("Input schuld not be empty");
+        return;
+    }
+    createTask();
+    input.value="";
+})
+
 
